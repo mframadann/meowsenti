278bfd3aaf01b06b8752d718818c438cea77cd61
Muhamad Firly Ramadan <ramadan@Ramadan-Mac.local>
2025-02-07 01:13:11 +0700

    deploy to huggingface

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/v1', methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
     resp = {
         "message": "All service working normally.",
